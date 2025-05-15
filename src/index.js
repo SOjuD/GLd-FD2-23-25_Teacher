dce014b5a13c99fc69b0dffe3c1f6ac0eaef49ab
Aleh Siarheichyk <44555176+SOjuD@users.noreply.github.com>
2025-05-15 20:34:31 +0300

    add finish with module configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import './styles/main.scss'
+
+import SVG from '@Public/assets/test.svg'
+
+console.log(SVG)
+
 console.log('sdfgjkll')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+console.log(process.env.MODE)
 
 module.exports = {
     mode: "development",
@@ -16,11 +19,52 @@ module.exports = {
     },
     devtool: 'source-map',
     resolve: {
-        extensions: ['.js', '.css', '.scss']
+        extensions: ['.js'],
+        alias: {
+            "@Public": path.resolve(__dirname, 'public'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }],
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+            }
+        ]
     },
     plugins: [new HtmlWebpackPlugin({
         template: path.resolve(__dirname, './public/index.html'),
         inject: 'body',
         title: "Webpack App",
-    })]
+    }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })]
 }
